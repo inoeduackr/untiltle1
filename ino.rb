8e01acbe1dba739bfcbf9be703954bdc0d6c6aff
John Doe <johndoe@example.com>
2017-07-15 11:18:48 +0900

    1st commit

---
+++ ino.rb
@@ -0,0 +1,7 @@
+p3_input = 'You’ve probably already used many of the applications that were built with Ruby on Rails: Basecamp, GitHub, Shopify, Airbnb, Twitch, SoundCloud, Hulu, Zendesk, Square, Highrise. Those are just some of the big names, but there are literally hundreds of thousands of applications built with the framework since its release in 2004."'
+def p3(input)
+
+  each_char
+
+end
+p3(p3_input)
